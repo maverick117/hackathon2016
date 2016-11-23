647ccd06de3355ef15ac944f9b1142c733f9cccf
Min <philip980507@msn.cn>
2016-11-23 20:01:26 +0800

    fix

--- Edison-py/serial/serial_test.py
+++ Edison-py/serial/serial_test.py
@@ -3,7 +3,6 @@ ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.write("testing")
 try:
     while 1:
-        
             response = ser.readline()
             if ';' not in response:
                 continue
@@ -12,8 +11,5 @@ try:
             #flowrate=int(res[1])
             print type(response)
             print response
-            l=response.split(';')
-            print 'l0=',l[0] 
-            print 'l1=',int(l[1])
 except KeyboardInterrupt:
      ser.close()
