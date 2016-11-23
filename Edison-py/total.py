c285d63bc0eb2ed814a5280d5f51603a4a0af4d5
Min <philip980507@msn.cn>
2016-11-23 20:34:01 +0800

    Tutoral

--- Edison-py/total.py
+++ Edison-py/total.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 from upm import pyupm_i2clcd as lcd
 from upm import pyupm_rfr359f as upmRfr359f
-from upm import pyupm_rpr220 as upmRpr220
 from upm import pyupm_grove as grove
 from upm import pyupm_mq303a as upmMq303a
 from upm import pyupm_buzzer as upmBuzzer
@@ -41,8 +40,6 @@ myDistInterrupter1 = upmRfr359f.RFR359F(2)
 myDistInterrupter2 = upmRfr359f.RFR359F(3)
 myAlcoholSensor = upmMq303a.MQ303A(0, 1)
 button = grove.GroveButton(4)
-#myReflectiveSensor1 = upmRpr220.RPR220(4)
-#myReflectiveSensor2 = upmRpr220.RPR220(5)
 warmed=False
 space=' '*16
 starttime=time.time()
